e7ffee87ba3d5815673248b32f6892d1c5d4ba3d
Rishi Natraj <64002170@ep-student.org>
2020-11-23 10:42:06 -0600

    main menu

--- Infection Tag/MainMenu.swift
+++ Infection Tag/MainMenu.swift
@@ -13,10 +13,10 @@ class MainMenu: SKScene {
 var buttonPlay: MSButtonNode!
 
     override func didMove(to view: SKView) {
+        buttonPlay = (self.childNode(withName: "buttonPlay") as! MSButtonNode)
         buttonPlay.selectedHandler = {
             self.loadGame()
         }
-        buttonPlay = (self.childNode(withName: "buttonPlay") as! MSButtonNode)
         
 
     }
@@ -33,14 +33,6 @@ var buttonPlay: MSButtonNode!
             print("Could not make GameScene, check the name is spelled correctly")
             return
         }
-
-        /* 3) Ensure correct aspect mode */
-        scene.scaleMode = .aspectFill
-
-        /* Show debug */
-        skView.showsPhysics = true
-        skView.showsDrawCount = true
-        skView.showsFPS = true
 
         /* 4) Start game scene */
         skView.presentScene(scene)
