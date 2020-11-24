93d66ef57da31873012cd9f13988a4400ce17477
Rishi Natraj <64002170@ep-student.org>
2020-11-24 10:31:42 -0600

    location of image array

--- Infection Tag/Character.swift
+++ Infection Tag/Character.swift
@@ -16,6 +16,22 @@ class Character: SKSpriteNode {
     var id: String
     var isInfected: Bool
     init(isInfected: Bool, ID: String) {
+        walkSprites.append(SKTexture(imageNamed: "walk1"))
+        walkSprites.append(SKTexture(imageNamed: "walk2"))
+        walkSprites.append(SKTexture(imageNamed: "walk3"))
+        walkSprites.append(SKTexture(imageNamed: "walk4"))
+        walkSprites.append(SKTexture(imageNamed: "walk5"))
+        walkSprites.append(SKTexture(imageNamed: "walk6"))
+        walkSprites.append(SKTexture(imageNamed: "walk7"))
+        walkSprites.append(SKTexture(imageNamed: "walk8"))
+        ZwalkSprites.append(SKTexture(imageNamed: "Zwalk1"))
+        ZwalkSprites.append(SKTexture(imageNamed: "Zwalk2"))
+        ZwalkSprites.append(SKTexture(imageNamed: "Zwalk3"))
+        ZwalkSprites.append(SKTexture(imageNamed: "Zwalk4"))
+        ZwalkSprites.append(SKTexture(imageNamed: "Zwalk5"))
+        ZwalkSprites.append(SKTexture(imageNamed: "Zwalk6"))
+        ZwalkSprites.append(SKTexture(imageNamed: "Zwalk7"))
+        ZwalkSprites.append(SKTexture(imageNamed: "Zwalk8"))
         self.id = ID
         // Make a texture from an image, a color, and size
         let texture = walkSprites[2]
--- Infection Tag/GameViewController.swift
+++ Infection Tag/GameViewController.swift
@@ -28,22 +28,7 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         
-        walkSprites.append(SKTexture(imageNamed: "walk1"))
-        walkSprites.append(SKTexture(imageNamed: "walk2"))
-        walkSprites.append(SKTexture(imageNamed: "walk3"))
-        walkSprites.append(SKTexture(imageNamed: "walk4"))
-        walkSprites.append(SKTexture(imageNamed: "walk5"))
-        walkSprites.append(SKTexture(imageNamed: "walk6"))
-        walkSprites.append(SKTexture(imageNamed: "walk7"))
-        walkSprites.append(SKTexture(imageNamed: "walk8"))
-        ZwalkSprites.append(SKTexture(imageNamed: "Zwalk1"))
-        ZwalkSprites.append(SKTexture(imageNamed: "Zwalk2"))
-        ZwalkSprites.append(SKTexture(imageNamed: "Zwalk3"))
-        ZwalkSprites.append(SKTexture(imageNamed: "Zwalk4"))
-        ZwalkSprites.append(SKTexture(imageNamed: "Zwalk5"))
-        ZwalkSprites.append(SKTexture(imageNamed: "Zwalk6"))
-        ZwalkSprites.append(SKTexture(imageNamed: "Zwalk7"))
-        ZwalkSprites.append(SKTexture(imageNamed: "Zwalk8"))
+        
         let theScene = scene
         let skView = view as! SKView
         skView.presentScene(theScene)
