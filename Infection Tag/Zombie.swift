385ce1245cd7461dc53859033905d272f7ebda12
Sarthak Agrawal <sarthavader@gmail.com>
2020-12-08 10:40:09 -0600

    increased turn lag

--- Infection Tag/Zombie.swift
+++ Infection Tag/Zombie.swift
@@ -26,7 +26,7 @@ class Zombie: Character {
     
     func move() {
         let perfectAng = -atan2((self.character.position.x - self.position.x), (self.character.position.y - self.position.y))
-        self.zRotation = (perfectAng - self.angle)/10 + self.angle
+        self.zRotation = (perfectAng - self.angle)/100 + self.angle
         self.position.x = self.position.x + (4 * cos(zRotation+CGFloat(Float.pi/2)))
         self.position.y = self.position.y + (4 * sin(zRotation+CGFloat(Float.pi/2)))
         self.angle = self.zRotation
